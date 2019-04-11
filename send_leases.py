3e2f3a819ed9b8a1c703579016e02bbd1d8cec1d
Nathan Whetsell <nathan.whetsell@gmail.com>
2019-04-11 13:50:17 -0400

    Vary email based on availability of guarantor

--- send_leases.py
+++ send_leases.py
@@ -11,7 +11,7 @@ import json
 import os
 import smtplib
 import sys
-import textwrap
+from textwrap import dedent
 
 
 with open('info.json') as file:
@@ -20,36 +20,6 @@ with open('info.json') as file:
 date_template = '{date:%B} {date.day}, {date.year}'
 lease_start_date = date_template.format(date=datetime.strptime(info['Lease start date'], '%Y-%m-%d'))
 security_deposit_due_date = datetime.strptime(info['Security deposit due date'], '%Y-%m-%d')
-
-message_template = '''{{tenant_first_name}},
-
-Please find attached your lease for {lease_description}, a copy of the EPA’s pamphlet “Protect Your Family From Lead in Your Home”, and a move-in form.
-
-Before signing the lease, you and {{guarantor_name}} (your guarantor) should read it carefully. To avoid late fees and ensure you’ll be able to move in on {lease_start_date}, here are a few things you must do:
-
-1. Deliver your signed lease to High Pi {Alumni_Advisor_name} by {lease_due_date}. You and your guarantor must sign the signature page. In addition, please remember to initial items (c) and (d) of and sign the Disclosure of Information on Lead-Based Paint form on the last page of the lease.
-
-2. If this will be your first time living in the chapter house, on {security_deposit_invoice_date} you’ll be emailed an invoice for your ${security_deposit} security deposit. Please pay this invoice by {security_deposit_due_date}. If you’ve lived in the chapter house before, we’ll apply your previous security deposit to this lease.
-
-3. On {first_rent_invoice_date} you’ll be emailed an invoice for your first month’s rent. Please pay this invoice by {lease_start_date}.
-
-Please let me know if you have any questions.
-
-{name}
-{title}
-
-'''.format(
-    lease_description             = info['Lease description'].lower(),
-    lease_start_date              = lease_start_date,
-    Alumni_Advisor_name           = info['Alumni Advisor name'],
-    lease_due_date                = date_template.format(date=datetime.strptime(info['Lease due date'], '%Y-%m-%d')),
-    security_deposit_invoice_date = security_deposit_due_date.strftime('%B 1, %Y'),
-    security_deposit              = '{:,.2f}'.format(info['Security deposit']),
-    security_deposit_due_date     = date_template.format(date=security_deposit_due_date),
-    first_rent_invoice_date       = datetime.strptime(info['Lease start date'], '%Y-%m-%d').strftime('%B 1, %Y'),
-    name                          = info['Name'],
-    title                         = info['Title']
-)
 
 common_attachments = []
 for path in [os.path.join('support', 'lead_in_your_home_brochure_land_b_w_508_easy_print_0.pdf'), 'Move-In Form.rtf']:
@@ -85,7 +55,7 @@ else:
         SMTP_host = 'smtp.mail.umich.edu'
         SMTP_port = 587
     else:
-        sys.exit(textwrap.dedent('''
+        sys.exit(dedent('''\
             To send leases, add an SMTP host and port to info.json, like this:
 
                 "Email address": "{}",
@@ -114,10 +84,49 @@ with open('tenants.csv') as csv_file:
         message['To'] = row['Email address']
         message['Cc'] = info['Alumni Advisor email address']
         message['Subject'] = 'Sigma {} Lease'.format(info['Lease description'])
-        message.attach(MIMEText(message_template.format(
-            tenant_first_name = tenant_first_name,
-            guarantor_name    = row['Guarantor name'].strip()
-        ), 'plain'))
+
+        verbose_signers = 'you'
+        brief_signers = 'you'
+        guarantor_name = row['Guarantor name'].strip()
+        if guarantor_name:
+            verbose_signers += ' and {} (your guarantor)'.format(guarantor_name)
+            brief_signers += ' and your guarantor'
+
+        message_text = dedent('''\
+            {tenant_first_name},
+
+            Please find attached your lease for {lease_description}, a copy of the EPA’s pamphlet “Protect Your Family From Lead in Your Home”, and a move-in form.
+
+            Before signing the lease, {verbose_signers} should read it carefully. To avoid late fees and ensure you’ll be able to move in on {lease_start_date}, here are a few things you must do:
+
+            1. Deliver your signed lease to Alumni Advisor {Alumni_Advisor_name} (email to {Alumni_Advisor_email_address} is preferred) by {lease_due_date}. {Brief_signers} must sign the signature page. In addition, please remember to initial items (c) and (d) of and sign the Disclosure of Information on Lead-Based Paint form on the last page of the lease.
+
+            2. If this will be your first time living in the Lambda Chi Alpha chapter house, on {security_deposit_invoice_date} you’ll be emailed an invoice for your ${security_deposit} security deposit. Please pay this invoice by {security_deposit_due_date}. If you’ve lived in the Lambda Chi Alpha chapter house before, we’ll apply your previous security deposit to this lease.
+
+            3. On {first_rent_invoice_date} you’ll be emailed an invoice for your first month’s rent. Please pay this invoice by {lease_start_date}.
+
+            Please let me know if you have any questions.
+
+            {name}
+            {title}
+
+        ''').format(
+            tenant_first_name             = tenant_first_name,
+            lease_description             = info['Lease description'].lower(),
+            verbose_signers               = verbose_signers,
+            lease_start_date              = lease_start_date,
+            Alumni_Advisor_name           = info['Alumni Advisor name'],
+            Alumni_Advisor_email_address  = info['Alumni Advisor email address'],
+            lease_due_date                = date_template.format(date=datetime.strptime(info['Lease due date'], '%Y-%m-%d')),
+            Brief_signers                 = brief_signers.capitalize(),
+            security_deposit_invoice_date = security_deposit_due_date.strftime('%B 1, %Y'),
+            security_deposit              = '{:,.2f}'.format(info['Security deposit']),
+            security_deposit_due_date     = date_template.format(date=security_deposit_due_date),
+            first_rent_invoice_date       = datetime.strptime(info['Lease start date'], '%Y-%m-%d').strftime('%B 1, %Y'),
+            name                          = info['Name'],
+            title                         = info['Title']
+        )
+        message.attach(MIMEText(message_text, 'plain'))
 
         path = os.path.join('leases', '{} {} {} Lease.pdf'.format(tenant_first_name, tenant_last_name, info['Lease description']))
         with open(path, 'rb') as attached_file:
