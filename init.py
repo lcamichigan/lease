55af145c717b9fa727478d9eedc57d801a09af2c
Neal Turett <turettn@gmail.com>
2021-05-25 19:51:38 -0600

    Consent to sign electronically

--- init.py
+++ init.py
@@ -5,7 +5,7 @@ import csv
 from datetime import date
 import os
 import subprocess
-import textwrap
+import json
 
 # http://docs.python-requests.org/en/master/
 import requests
@@ -13,34 +13,35 @@ import requests
 
 file_path = 'info.json'
 if not os.path.exists(file_path):
-    with open(file_path, 'w') as file:
-        current_year = date.today().year
-        next_year = current_year + 1
-        file.write(textwrap.dedent('''\
-        {{
+    current_year = date.today().year
+    next_year = current_year + 1
+
+    rent = 765
+
+    data = {
             "Name": "Your Name",
             "Title": "Your Title, Sigma Alumni Association",
             "Email address": "your.address@domain.com",
 
-            "Lease description": "Fall {current_year} and Winter {next_year}",
-            "Lease due date": "{current_year}-04-01",
-            "Lease start date": "{current_year}-08-15",
-            "Lease end date": "{next_year}-05-15",
+            "Lease description": "Fall %d and Winter %d" % (current_year, next_year),
+            "Lease due date": "%d-04-01" % current_year,
+            "Lease start date": "%d-08-15" % current_year,
+            "Lease end date": "%d-05-15" % next_year,
             "Rent": [
-                ["{current_year}-08-15", 740],
-                ["{current_year}-09-15", 740],
-                ["{current_year}-10-15", 740],
-                ["{current_year}-11-15", 740],
-                ["{current_year}-12-15", 740],
-                ["{next_year}-01-15", 740],
-                ["{next_year}-02-15", 740],
-                ["{next_year}-03-15", 740],
-                ["{next_year}-04-15", 740]
+                ["%d-08-15" % current_year, rent],
+                ["%d-09-15" % current_year, rent],
+                ["%d-10-15" % current_year, rent],
+                ["%d-11-15" % current_year, rent],
+                ["%d-12-15" % current_year, rent],
+                ["%d-01-15" % next_year, rent],
+                ["%d-02-15" % next_year, rent],
+                ["%d-03-15" % next_year, rent],
+                ["%d-04-15" % next_year, rent]
             ],
-            "Monthly rent during holdover": 1110,
+            "Monthly rent during holdover": int(rent * 1.5),
 
             "Security deposit": 500,
-            "Security deposit due date": "{current_year}-05-15",
+            "Security deposit due date": "%d-05-15" % current_year,
             "Security deposit location": "ABC Bank, 123 Main St, Anywhere MI 00000-0000",
 
             "Alumni Advisor name": "John Mason",
@@ -49,11 +50,10 @@ if not os.path.exists(file_path):
 
             "Sigma signatory name": "Warren Cole",
             "Sigma signatory title": "President"
-        }}
-        '''.format(
-            current_year = current_year,
-            next_year    = next_year
-        )))
+        }
+
+    with open(file_path, 'w') as fout:
+        json.dump(data, fout, indent=4)
 
 file_path = 'tenants.csv'
 if not os.path.exists(file_path):
@@ -71,19 +71,23 @@ if not os.path.exists(directory_name):
 # https://www.epa.gov/lead/protect-your-family-lead-your-home-real-estate-disclosure
 file_path = os.path.join(directory_name, 'lead_in_your_home_brochure_land_b_w_508_easy_print_0.pdf')
 if not os.path.exists(file_path):
+    req = requests.get('https://www.epa.gov/sites/production/files/2014-02/documents/' + os.path.basename(file_path))
+    req.raise_for_status()
     with open(file_path, 'wb') as file:
-        file.write(requests.get('https://www.epa.gov/sites/production/files/2014-02/documents/' + os.path.basename(file_path)).content)
+        file.write(req.content)
 
 # https://www.epa.gov/lead/sellers-disclosure-information-lead-based-paint-andor-lead-based-paint-hazards
 file_path = os.path.join(directory_name, 'lesr_eng.pdf')
 if not os.path.exists(file_path):
+    req = requests.get('https://www.epa.gov/sites/production/files/documents/' + os.path.basename(file_path))
+    req.raise_for_status()
     with open(file_path, 'wb') as file:
-        file.write(requests.get('https://www.epa.gov/sites/production/files/documents/' + os.path.basename(file_path)).content)
+        file.write(req.content)
 
 jobname = 'SigmaSignature'
 file_path = os.path.join(directory_name, jobname + '.pdf')
 if not os.path.exists(file_path):
-    subprocess.check_call([
+    cmd = [
         'lualatex',
         '-interaction=batchmode',
         '-jobname=' + jobname,
@@ -98,4 +102,6 @@ if not os.path.exists(file_path):
             'Replace ' + file_path.replace('\\', '\\textbackslash ') + '\par with an image of a signature.'
             '\end{document}'
         )
-    ])
+    ]
+
+    subprocess.check_call(cmd)
